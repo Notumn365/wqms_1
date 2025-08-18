6dc072a45c83c644f3af8b75a9c0d8a7c2a7f891
John Doe <weiqiu365@foxmail.com>
2025-08-19 00:01:58 +0800

    2025_8_19_01

---
+++ src/main/java/com/weiqiu/wqmso/controller/PostThumbController.java
@@ -0,0 +1,55 @@
+package com.weiqiu.wqmso.controller;
+
+import com.weiqiu.wqmso.common.BaseResponse;
+import com.weiqiu.wqmso.common.ErrorCode;
+import com.weiqiu.wqmso.common.ResultUtils;
+import com.weiqiu.wqmso.exception.BusinessException;
+import com.weiqiu.wqmso.model.dto.postthumb.PostThumbAddRequest;
+import com.weiqiu.wqmso.model.entity.User;
+import com.weiqiu.wqmso.service.PostThumbService;
+import com.weiqiu.wqmso.service.UserService;
+import javax.annotation.Resource;
+import javax.servlet.http.HttpServletRequest;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * 帖子点赞接口
+ *
+ *
+ */
+@RestController
+@RequestMapping("/post_thumb")
+@Slf4j
+public class PostThumbController {
+
+    @Resource
+    private PostThumbService postThumbService;
+
+    @Resource
+    private UserService userService;
+
+    /**
+     * 点赞 / 取消点赞
+     *
+     * @param postThumbAddRequest
+     * @param request
+     * @return resultNum 本次点赞变化数
+     */
+    @PostMapping("/")
+    public BaseResponse<Integer> doThumb(@RequestBody PostThumbAddRequest postThumbAddRequest,
+            HttpServletRequest request) {
+        if (postThumbAddRequest == null || postThumbAddRequest.getPostId() <= 0) {
+            throw new BusinessException(ErrorCode.PARAMS_ERROR);
+        }
+        // 登录才能点赞
+        final User loginUser = userService.getLoginUser(request);
+        long postId = postThumbAddRequest.getPostId();
+        int result = postThumbService.doPostThumb(postId, loginUser);
+        return ResultUtils.success(result);
+    }
+
+}
---
+++ src/main/java/com/weiqiu/wqmso/exception/GlobalExceptionHandler.java
@@ -0,0 +1,30 @@
+package com.weiqiu.wqmso.exception;
+
+import com.weiqiu.wqmso.common.BaseResponse;
+import com.weiqiu.wqmso.common.ErrorCode;
+import com.weiqiu.wqmso.common.ResultUtils;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+/**
+ * 全局异常处理器
+ *
+ *
+ */
+@RestControllerAdvice
+@Slf4j
+public class GlobalExceptionHandler {
+
+    @ExceptionHandler(BusinessException.class)
+    public BaseResponse<?> businessExceptionHandler(BusinessException e) {
+        log.error("BusinessException", e);
+        return ResultUtils.error(e.getCode(), e.getMessage());
+    }
+
+    @ExceptionHandler(RuntimeException.class)
+    public BaseResponse<?> runtimeExceptionHandler(RuntimeException e) {
+        log.error("RuntimeException", e);
+        return ResultUtils.error(ErrorCode.SYSTEM_ERROR, "系统错误");
+    }
+}
