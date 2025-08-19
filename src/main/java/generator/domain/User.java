3da285f25ec4080aabdc1dcce5a263608bb85fed
John Doe <weiqiu365@foxmail.com>
2025-08-19 14:01:45 +0800

    2025_8_19_03

---
+++ src/main/java/generator/domain/User.java
@@ -0,0 +1,107 @@
+package generator.domain;
+
+import com.baomidou.mybatisplus.annotation.IdType;
+import com.baomidou.mybatisplus.annotation.TableField;
+import com.baomidou.mybatisplus.annotation.TableId;
+import com.baomidou.mybatisplus.annotation.TableName;
+import java.util.Date;
+import lombok.Data;
+
+/**
+ * 用户
+ * @TableName user
+ */
+@TableName(value ="user")
+@Data
+public class User {
+    /**
+     * id
+     */
+    @TableId(type = IdType.AUTO)
+    private Long id;
+
+    /**
+     * 账号
+     */
+    private String userAccount;
+
+    /**
+     * 密码
+     */
+    private String userPassword;
+
+    /**
+     * 微信开放平台id
+     */
+    private String unionId;
+
+    /**
+     * 公众号openId
+     */
+    private String mpOpenId;
+
+    /**
+     * 用户昵称
+     */
+    private String userName;
+
+    /**
+     * 用户头像
+     */
+    private String userAvatar;
+
+    /**
+     * 用户简介
+     */
+    private String userProfile;
+
+    /**
+     * 用户角色：user/admin/ban
+     */
+    private String userRole;
+
+    /**
+     * 编辑时间
+     */
+    private Date editTime;
+
+    /**
+     * 创建时间
+     */
+    private Date createTime;
+
+    /**
+     * 更新时间
+     */
+    private Date updateTime;
+
+    /**
+     * 是否删除
+     */
+    private Integer isDelete;
+
+    /**
+     * 手机号
+     */
+    private String phoneNumber;
+
+    /**
+     * 邮箱
+     */
+    private String email;
+
+    /**
+     * 年级
+     */
+    private String grade;
+
+    /**
+     * 工作经验
+     */
+    private String workExperience;
+
+    /**
+     * 擅长方向
+     */
+    private String expertiseDirection;
+}
---
+++ src/main/java/generator/service/QuestionBankQuestionService.java
@@ -0,0 +1,13 @@
+package generator.service;
+
+import generator.domain.QuestionBankQuestion;
+import com.baomidou.mybatisplus.extension.service.IService;
+
+/**
+* @author 24905
+* @description 针对表【question_bank_question(题库题目)】的数据库操作Service
+* @createDate 2025-08-19 13:57:20
+*/
+public interface QuestionBankQuestionService extends IService<QuestionBankQuestion> {
+
+}
