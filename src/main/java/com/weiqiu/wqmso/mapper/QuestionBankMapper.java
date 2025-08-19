ac38dbb15fa62be50d062f5c2ce43f1f1378807d
John Doe <weiqiu365@foxmail.com>
2025-08-19 14:34:13 +0800

    2025_8_19_04

--- src/main/java/com/weiqiu/wqmso/mapper/QuestionBankMapper.java
+++ src/main/java/com/weiqiu/wqmso/mapper/QuestionBankMapper.java
@@ -1,13 +1,13 @@
-package generator.mapper;
+package com.weiqiu.wqmso.mapper;
 
-import generator.domain.QuestionBank;
+import com.weiqiu.wqmso.model.entity.QuestionBank;
 import com.baomidou.mybatisplus.core.mapper.BaseMapper;
 
 /**
 * @author 24905
 * @description 针对表【question_bank(题库)】的数据库操作Mapper
 * @createDate 2025-08-19 13:57:20
-* @Entity generator.domain.QuestionBank
+* @Entity com.weiqiu.wqmso.model.entity.QuestionBank
 */
 public interface QuestionBankMapper extends BaseMapper<QuestionBank> {
 
--- src/main/java/com/weiqiu/wqmso/mapper/QuestionBankQuestionMapper.java
+++ src/main/java/com/weiqiu/wqmso/mapper/QuestionBankQuestionMapper.java
@@ -1,13 +1,13 @@
-package generator.mapper;
+package com.weiqiu.wqmso.mapper;
 
-import generator.domain.QuestionBankQuestion;
+import com.weiqiu.wqmso.model.entity.QuestionBankQuestion;
 import com.baomidou.mybatisplus.core.mapper.BaseMapper;
 
 /**
 * @author 24905
 * @description 针对表【question_bank_question(题库题目)】的数据库操作Mapper
 * @createDate 2025-08-19 13:57:20
-* @Entity generator.domain.QuestionBankQuestion
+* @Entity com.weiqiu.wqmso.model.entity.QuestionBankQuestion
 */
 public interface QuestionBankQuestionMapper extends BaseMapper<QuestionBankQuestion> {
 
